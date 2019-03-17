486967b7dd18d5e172229ceffda7e36b51ddc6bd
hugo <hugo.denisse@gmail.com>
2019-03-17 02:24:53 +0100

    move server stuffs to server folder

--- sagas/posts.js
+++ sagas/posts.js
@@ -1,20 +1,20 @@
 import { takeLatest, put } from 'redux-saga/effects';
 import { FETCH_POST, fetchPostSuccess, fetchPostError } from '../actions/posts';
-import fetch from 'isomorphic-unfetch';
+import Api from '../lib/api';
 
 function* doFetchPost({ id, ctx }) {
-  const res = yield fetch(`http://localhost:3000/api/posts/${id}`);
-  if (res.ok) {
-    const post = yield res.json();
-    yield put(fetchPostSuccess(post));
-  } else {
-    const message = yield res.text();
-    const code = res.status;
-    if (ctx.isServer) {
-      ctx.res.statusCode = code;
-    }
-    yield put(fetchPostError({ code, message }));
-  }
+  const r = yield Api.get(`/posts/${id}`)
+    .then(json => {
+      return put(fetchPostSuccess(json));
+    })
+    .catch(({ status, message }) => {
+      console.log('err', { status, message });
+      if (ctx.isServer) {
+        ctx.res.statusCode = status;
+      }
+      return put(fetchPostError({ status, message }));
+    });
+  yield r;
 }
 
 export default function* authSaga() {
